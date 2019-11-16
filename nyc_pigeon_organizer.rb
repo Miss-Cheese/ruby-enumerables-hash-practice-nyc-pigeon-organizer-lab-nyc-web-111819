3be6532a41d585aee4f1bc0fc80585d5d7128307
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 14:53:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,7 +20,8 @@ names_array = data[:color][:grey]
 names_array.each  {|pigeon_name|
 
   local_hash_value = pigeon_hash[pigeon_name]
-  local_hash_value
+  if local_hash_value == nil
+  end
 pigeon_hash[pigeon_name] = {:color => ["grey"]}
 }
 
