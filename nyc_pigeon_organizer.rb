d74c2459a2f521db42ceb2e28533362451467a1d
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 15:05:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # Iterate over the hash above, collecting each pigeon by name
-  # and insert it as the key of a new hash.
-  #
-  # Each of these hashes should have :color, :gender, and :lives keys
-  # assigned to arrays of info about that particular pigeon
+
 
 pigeon_hash = {}
 
-
-
 names_array = data[:color][:purple]
+
+
 
 names_array.each  {|pigeon_name|
 pigeon_hash[pigeon_name] = {:color => ["purple"]}
@@ -25,15 +21,12 @@ names_array.each  {|pigeon_name|
     pigeon_hash[pigeon_name] = local_hash_value
   end
 
-  # local_hash_value = {:color => ["grey"]}
 
   if !local_hash_value{:color}
     local_hash_value[:color] = []
   end
 
   local_hash_value[:color] << "grey"
-
-
 
 }
 
