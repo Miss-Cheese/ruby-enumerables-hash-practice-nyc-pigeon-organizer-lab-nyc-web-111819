398b5fc23c2556e716607c8594c261afa5a879c8
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 15:20:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,12 +9,29 @@ pigeon_color(pigeon_hash, data[:color][:brown], "brown")
 
   puts "1------------------------"
   pp pigeon_hash
-
   puts "2------------------------"
 end
 
 
 def pigeon_color(pigeon_hash, color_name_array, color)
+
+  color_name_array.each  {|pigeon_name|
+
+    local_hash_value = pigeon_hash[pigeon_name]
+    if local_hash_value == nil
+      local_hash_value = {}
+      pigeon_hash[pigeon_name] = local_hash_value
+    end
+
+
+    if !local_hash_value[:color]
+      local_hash_value[:color] = []
+    end
+
+    local_hash_value[:color] << color
+
+  }
+
 
   color_name_array.each  {|pigeon_name|
 
