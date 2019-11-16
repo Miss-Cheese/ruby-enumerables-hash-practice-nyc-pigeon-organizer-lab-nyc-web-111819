fd99fb4f2fb4d2159a328986f788224b0264aabd
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 15:44:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,6 +10,11 @@ pigeon_color(pigeon_hash, data[:color][:brown], "brown")
 pigeon_gender(pigeon_hash, data[:gender][:female], "female")
 pigeon_gender(pigeon_hash, data[:gender][:male], "male")
 
+pigeon_locale(pigeon_hash, data[:lives]["Subway"], "Subway")
+pigeon_locale(pigeon_hash, data[:lives]["Central Park"], "Central Park")
+pigeon_locale(pigeon_hash, data[:lives]["Library"], "Library")
+pigeon_locale(pigeon_hash, data[:lives]["City Hall"], "City Hall")
+
   puts "1------------------------"
   pp pigeon_hash
   puts "2------------------------"
