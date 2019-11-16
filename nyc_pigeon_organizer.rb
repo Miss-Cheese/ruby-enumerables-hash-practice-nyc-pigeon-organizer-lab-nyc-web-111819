9cea3026253a0d8a0d35a192991e612b826c18c8
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 15:16:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,35 +3,13 @@ def nyc_pigeon_organizer(data)
 
 pigeon_hash = {}
 
-names_array = data[:color][:purple]
+pigeon_color(pigeon_hash, data[:color][:purple], "purple")
 
+pigeon_color(pigeon_hash, data[:color][:grey], "grey")
 
+pigeon_color(pigeon_hash, data[:color][:white], "white")
 
-names_array.each  {|pigeon_name|
-pigeon_hash[pigeon_name] = {:color => ["purple"]}
-}
-
-
-
-names_array = data[:color][:grey]
-
-names_array.each  {|pigeon_name|
-
-  local_hash_value = pigeon_hash[pigeon_name]
-  if local_hash_value == nil
-    local_hash_value = {}
-    pigeon_hash[pigeon_name] = local_hash_value
-  end
-
-
-  if !local_hash_value[:color]
-    local_hash_value[:color] = []
-  end
-
-  local_hash_value[:color] << "grey"
-
-}
-
+pigeon_color(pigeon_hash, data[:color][:brown], "brown")
 
   puts "1------------------------"
   pp pigeon_hash
